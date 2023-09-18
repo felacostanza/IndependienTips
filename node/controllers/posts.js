050f81fbdf6c66d3d7c37a528e9c5fccfa890f41
felacostanza <felipeacostanza@gmail.com>
2023-09-18 19:56:54 -0300

    otro commit

---
+++ node/controllers/posts.js
@@ -0,0 +1,19 @@
+export const getAllBlogs = (req, res) => {
+
+}
+
+export const getSingleBlog = (req, res) => {
+
+}
+
+export const postBlog = (req, res) => {
+
+}
+
+export const updateBlog = (req, res) => {
+
+}
+
+export const deleteBlog = (req, res) => {
+
+}
